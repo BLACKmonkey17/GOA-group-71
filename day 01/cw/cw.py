ff1ea5207bd67551fdd978db03c6a081600dd207
BLACKmonkey17 <iuriimerlishvii3@gmail.com>
2025-04-23 21:39:56 +0400

    first commit

---
+++ day 01/cw/cw.py
@@ -0,0 +1,8 @@
+#printi() funqcias viyenebt terminalshi chveni dawerili kodis shedegi rom gamovitanot
+city = "tokyo"
+country = "japan"
+food = "mwvadi"
+drink = "water"
+print(city, country, food, drink)
+#print() gamoaqvs shedegi terminalshi. variable-inaxavs mnishvnelobas. int-mteli ricxvebi.
+#  booleans- True False. float- wiladi ricxvebi
