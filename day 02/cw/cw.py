368d227e5a7aa4ceb6c4d4ad7f2c83aad3fb7f82
username <user@gmail.com>
2025-04-29 00:51:58 +0400

    first commit

---
+++ day 02/cw/cw.py
@@ -0,0 +1,11 @@
+# name = "iuri"
+# surname = "imerlishvili"
+# print(name,  surname)
+
+
+num1 = 100
+num2 = 10
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
---
+++ day 02/hw/hw.py
@@ -0,0 +1,72 @@
+#დეტალურად აღწერეთ თუ რა განსხვავებაა ბრჭყალებში ჩაწერილ რიცხვსა და ჩვეულებრივ ინტეჯერ რიცხვს შორის?
+#"1" aris stringi da araris aritmetika shesadzlebeli da aseve chven shegvidzlia amis setqstan mushaobashi gamoyeneba.
+# 1 aris  integeri,ricxvi. chven shegvidzlia gamoviyenot aritmetikashi. da gamoiyeneba matematikashi
+
+
+
+#დღეს ნასწავლ თითოეულ მათემატიკურ ოპერატორზე გააკეთეთ 5-5 მაგალითი სხვადასხვა რიცხვებით და თითოეულის შედეგი გამოიტანეთ ტერმინალში.
+
+num1 = 100
+num2 = 130
+num3 = 150
+num4 = 139
+num5 = 134
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+print(num4 + num3)
+print(num4 - num3)
+print(num4 * num3)
+print(num4 / num3)
+
+print(num5 + num2)
+print(num5 - num2)
+print(num4 * num2)
+print(num5 / num2)
+
+
+# შექმენით ორი ცვლადი და თითოეულში შეინახეთ რიცხვები. შექმენით ოთხი ცვლადი: 
+# • Addition
+# • Substraction
+# • Multiplication
+# • Division
+
+variable1 = 150
+variable2 = 100
+print(variable1 + variable2) #addition
+
+print(variable1 - variable2) #Substaction
+
+print(variable1 * variable2) #Multiplication
+
+print(variable1 / variable2) #Division
+
+addition = 250
+Substraction = 50
+Multiplication = 15000
+Division = 1.5
+
+                        # info that i know about python language
+#Among the features of Python, it is worth noting:
+
+# Extremely clear, understandable and readable syntax
+
+# Introspection
+
+# Multi-paradigm design with an object-oriented approach
+
+# Full modularity
+
+# Very high-level data typing
+
+# Metaprogramming
+
+# Extensive standard library
+
+# The python was created publicated in 1991 by: Guido Van Rosumi
+
+
+
